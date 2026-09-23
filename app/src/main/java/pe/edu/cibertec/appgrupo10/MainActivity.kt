a6846cfbd4a16763a5a74f1317051a91fdc8ed6c
luisV25 <villanuevachappaluisalberto@gamil.com>
2026-09-23 03:18:21 -0500

    Implementación Pregunta 7

--- app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
+++ app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
@@ -1,20 +1,27 @@
 package pe.edu.cibertec.appgrupo10
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnPregunta7 = findViewById<Button>(R.id.btnPregunta7)
+
+        btnPregunta7.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        if (v?.id == R.id.btnPregunta7) {
+            val intent = Intent(this, Pregunta7Activity::class.java)
+            startActivity(intent)
         }
     }
 }

7a91f36efef7f491f98b2fd6ddd737309ff5084b
luisV25 <villanuevachappaluisalberto@gamil.com>
2026-09-23 04:08:59 -0500

    Actualización del proyecto base

--- app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
+++ app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
@@ -1,27 +1,12 @@
 package pe.edu.cibertec.appgrupo10
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnPregunta7 = findViewById<Button>(R.id.btnPregunta7)
-
-        btnPregunta7.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-
-        if (v?.id == R.id.btnPregunta7) {
-            val intent = Intent(this, Pregunta7Activity::class.java)
-            startActivity(intent)
-        }
     }
 }

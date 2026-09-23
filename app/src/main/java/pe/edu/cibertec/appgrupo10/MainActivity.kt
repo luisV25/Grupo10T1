a2e4aea1bf5cf05239f25af4c463faa398a96b16
luisV25 <villanuevachappaluisalberto@gamil.com>
2026-09-23 04:34:46 -0500

    Implementación Pregunta 7

--- app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
+++ app/src/main/java/pe/edu/cibertec/appgrupo10/MainActivity.kt
@@ -2,26 +2,16 @@ package pe.edu.cibertec.appgrupo10
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btnPregunta7 = findViewById<Button>(R.id.btnPregunta7)
+        val intent = Intent(this, Pregunta7Activity::class.java)
+        startActivity(intent)
 
-        btnPregunta7.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-
-        if (v?.id == R.id.btnPregunta7) {
-            val intent = Intent(this, Pregunta7Activity::class.java)
-            startActivity(intent)
-        }
+        finish()
     }
 }
